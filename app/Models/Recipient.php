2d1242caa93771e46f600d70d77ca840c99d913c
serdimoa <serdimoa@gmail.com>
2016-06-18 10:27:49 +0500

    some

---
+++ app/Models/Recipient.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 16.06.2016
+ * Time: 13:57
+ */
+
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Recipient extends Model
+{
+
+}
